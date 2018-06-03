ceca11b94bb96a4bc4a81518f222508c46964131
Geaaru <geaaru@gmail.com>
2018-06-03 09:30:27 +0200

    Integrate cobra and viper

---
+++ cmd/root.go
@@ -0,0 +1,75 @@
+/*
+
+Copyright (C) 2017-2018  Ettore Di Giacinto <mudler@gentoo.org>
+                         Daniele Rondina <geaaru@sabayonlinux.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	common "github.com/MottainaiCI/mottainai-agent/common"
+	v "github.com/spf13/viper"
+)
+
+const (
+	agentName = `Mottainai Agent - Task/Job Agent
+Copyright (c) 2017-2018 Mottainai
+
+`
+	agentExamples = ""
+)
+
+var rootCmd = &cobra.Command{
+	Short:        agentName,
+	Version:      common.MAGENT_VERSION,
+	Example:      agentExamples,
+	Args:         cobra.OnlyValidArgs,
+	SilenceUsage: true,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func init() {
+	var pflags = rootCmd.PersistentFlags()
+	pflags.StringP("config", "c", common.MAGENT_DEF_CONFFILE, "Mottainai Agent Config File")
+
+	v.BindPFlag("config", rootCmd.PersistentFlags().Lookup("config"))
+
+	rootCmd.AddCommand(
+		newAgentCommand(),
+		newHealtcheckCommand(),
+	)
+}
+
+func Execute() {
+	// Start command execution
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ vendor/github.com/MottainaiCI/mottainai-server/pkg/mottainai/agent.go
@@ -0,0 +1,96 @@
+/*
+
+Copyright (C) 2018  Ettore Di Giacinto <mudler@gentoo.org>
+Credits goes also to Gogs authors, some code portions and re-implemented design
+are also coming from the Gogs project, which is using the go-macaron framework
+and was really source of ispiration. Kudos to them!
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+package mottainai
+
+import (
+	client "github.com/MottainaiCI/mottainai-server/pkg/client"
+	setting "github.com/MottainaiCI/mottainai-server/pkg/settings"
+	"github.com/mudler/anagent"
+
+	agenttasks "github.com/MottainaiCI/mottainai-server/pkg/tasks"
+	"github.com/MottainaiCI/mottainai-server/pkg/utils"
+	machinery "github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/log"
+)
+
+type MottainaiAgent struct {
+	*anagent.Anagent
+}
+
+func NewAgent() *MottainaiAgent {
+	return &MottainaiAgent{Anagent: anagent.New()}
+}
+
+func (m *MottainaiAgent) HealthCheckRun(config string) {
+	m.HealthCheckSetup(config)
+
+	m.Anagent.Start()
+}
+
+func (m *MottainaiAgent) Run(config string) error {
+
+	setting.GenDefault()
+	if len(config) > 0 {
+		setting.LoadFromFileEnvironment(config)
+	}
+
+	server := NewServer()
+	broker := server.Add(setting.Configuration.BrokerDefaultQueue)
+
+	th := agenttasks.DefaultTaskHandler()
+	m.Map(th)
+	ID := utils.GenID()
+	hostname := utils.Hostname()
+	log.INFO.Println("Worker ID: " + ID)
+	log.INFO.Println("Worker Hostname: " + hostname)
+
+	if setting.Configuration.PrivateQueue {
+		b := server.Add(hostname)
+		w := b.NewWorker(ID+hostname, 1)
+		log.INFO.Println("Listening on private queue")
+		go w.Launch()
+	}
+
+	defaultWorker := broker.NewWorker(ID, setting.Configuration.AgentConcurrency)
+	fetcher := client.NewClient()
+	fetcher.RegisterNode(ID, hostname)
+	m.Map(fetcher)
+
+	m.TimerSeconds(int64(200), true, func(c *client.Fetcher) {
+		c.RegisterNode(ID, hostname)
+	})
+
+	for q, concurrent := range setting.Configuration.Queues {
+		log.INFO.Println("Listening on queue ", q, " with concurrency ", concurrent)
+		b := server.Add(q)
+		w := b.NewWorker(ID, concurrent)
+		go w.Launch()
+	}
+
+	go func(w *machinery.Worker, a *MottainaiAgent) {
+		a.Map(w)
+		a.Start()
+	}(defaultWorker, m)
+
+	return defaultWorker.Launch()
+}
