67138dc6c354c1a4f793d797de7033954b74ff0e
Ettore Di Giacinto <mudler@gentoo.org>
2018-07-05 14:43:52 +0200

    update vendor/

---
+++ vendor/github.com/MottainaiCI/mottainai-server/pkg/context/context.go
@@ -0,0 +1,209 @@
+/*
+
+Copyright (C) 2017-2018  Ettore Di Giacinto <mudler@gentoo.org>
+Credits goes also to Gogs authors, some code portions and re-implemented design
+are also coming from the Gogs project, which is using the go-macaron framework
+and was really source of ispiration. Kudos to them!
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+package context
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	auth "github.com/MottainaiCI/mottainai-server/pkg/auth"
+	user "github.com/MottainaiCI/mottainai-server/pkg/user"
+
+	setting "github.com/MottainaiCI/mottainai-server/pkg/settings"
+	"github.com/go-macaron/cache"
+	"github.com/go-macaron/csrf"
+	"github.com/go-macaron/session"
+
+	macaron "gopkg.in/macaron.v1"
+)
+
+// Context represents context of a request.
+type Context struct {
+	*macaron.Context
+	Cache       cache.Cache
+	csrf        csrf.CSRF
+	Flash       *session.Flash
+	Session     session.Store
+	Link        string // Current request URL
+	IsLogged    bool
+	User        *user.User
+	IsBasicAuth bool
+}
+
+// HTML responses template with given status.
+func (c *Context) HTML(status int, name string) {
+	c.Context.HTML(status, name)
+}
+
+// Title sets "Title" field in template data.
+func (c *Context) Title(locale string) {
+	c.Data["Title"] = locale
+}
+
+// RenderWithErr used for page has form validation but need to prompt error to users.
+func (c *Context) RenderWithErr(msg, tpl string) {
+
+	c.Flash.ErrorMsg = msg
+	c.Data["Flash"] = c.Flash
+	c.HTML(http.StatusOK, tpl)
+}
+
+// RenderWithInfo used for page has form validation but need to prompt error to users.
+func (c *Context) RenderWithInfo(msg, tpl string) {
+
+	c.Flash.InfoMsg = msg
+	c.Data["Flash"] = c.Flash
+	c.HTML(http.StatusOK, tpl)
+}
+
+// ServerError renders the 500 page.
+func (c *Context) ServerError(title string, err error) {
+	c.Handle(http.StatusInternalServerError, title, err)
+}
+
+// Handle handles and logs error by given status.
+func (c *Context) Handle(status int, title string, err error) {
+	switch status {
+	case http.StatusNotFound:
+		c.Data["Title"] = "Page Not Found"
+	case http.StatusInternalServerError:
+		c.Data["Title"] = "Internal Server Error"
+		fmt.Println(3, "%s: %v", title, err)
+		if c.IsLogged && c.User.IsAdmin() {
+			c.Data["ErrorMsg"] = err
+		}
+	}
+	c.HTML(status, fmt.Sprintf("status/%d", status))
+}
+
+// FormErr sets "Err_xxx" field in template data.
+func (c *Context) FormErr(names ...string) {
+	for i := range names {
+		c.Data["Err_"+names[i]] = true
+	}
+}
+
+// HasError returns true if error occurs in form validation.
+func (c *Context) HasError() bool {
+	hasErr, ok := c.Data["HasError"]
+	if !ok {
+		return false
+	}
+	c.Flash.ErrorMsg = c.Data["ErrorMsg"].(string)
+	c.Data["Flash"] = c.Flash
+	return hasErr.(bool)
+}
+
+// PageIs sets "PageIsxxx" field in template data.
+func (c *Context) PageIs(name string) {
+	c.Data["PageIs"+name] = true
+}
+
+// Success responses template with status http.StatusOK.
+func (c *Context) Success(name string) {
+	c.HTML(http.StatusOK, name)
+}
+
+// JSONSuccess responses JSON with status http.StatusOK.
+func (c *Context) JSONSuccess(data interface{}) {
+	c.JSON(http.StatusOK, data)
+}
+
+// SubURLRedirect responses redirection wtih given location and status.
+// It prepends setting.AppSubURL to the location string.
+func (c *Context) SubURLRedirect(location string, status ...int) {
+	c.Redirect(setting.Configuration.AppSubURL + location)
+}
+
+func (c *Context) ServeContent(name string, r io.ReadSeeker, params ...interface{}) {
+	modtime := time.Now()
+	for _, p := range params {
+		switch v := p.(type) {
+		case time.Time:
+			modtime = v
+		}
+	}
+	c.Resp.Header().Set("Content-Description", "File Transfer")
+	c.Resp.Header().Set("Content-Type", "application/octet-stream")
+	c.Resp.Header().Set("Content-Disposition", "attachment; filename="+name)
+	c.Resp.Header().Set("Content-Transfer-Encoding", "binary")
+	c.Resp.Header().Set("Expires", "0")
+	c.Resp.Header().Set("Cache-Control", "must-revalidate")
+	c.Resp.Header().Set("Pragma", "public")
+	http.ServeContent(c.Resp, c.Req.Request, name, modtime, r)
+}
+
+func Setup(m *macaron.Macaron) {
+	m.Use(Contexter())
+}
+
+func Contexter() macaron.Handler {
+	return func(ctx *macaron.Context, sess session.Store, f *session.Flash, x csrf.CSRF, cache cache.Cache) {
+		c := &Context{
+			Context: ctx,
+			Cache:   cache,
+			csrf:    x,
+			Flash:   f,
+			Session: sess,
+			Link:    setting.Configuration.AppSubURL + strings.TrimSuffix(ctx.Req.URL.Path, "/"),
+		}
+		c.Data["Link"] = c.Link
+		c.Data["PageStartTime"] = time.Now()
+
+		if len(setting.Configuration.AccessControlAllowOrigin) > 0 {
+			// Set CORS headers for browser-based git clients
+			ctx.Resp.Header().Set("Access-Control-Allow-Origin", setting.Configuration.AccessControlAllowOrigin)
+			ctx.Resp.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+			ctx.Header().Set("Access-Control-Allow-Origin", setting.Configuration.AccessControlAllowOrigin)
+			c.Header().Set("'Access-Control-Allow-Credentials' ", "true")
+			c.Header().Set("Access-Control-Max-Age", "3600")
+			c.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+		}
+
+		// Get user from session if logined.
+		c.User, c.IsBasicAuth = auth.SignedInUser(c.Context, c.Session)
+
+		if c.User != nil {
+			c.IsLogged = true
+			c.Data["IsLogged"] = c.IsLogged
+			c.Data["LoggedUser"] = c.User
+			c.Data["LoggedUserID"] = c.User.ID
+			c.Data["LoggedUserName"] = c.User.Name
+			c.Data["IsAdmin"] = c.User.Admin
+		} else {
+			c.Data["LoggedUserID"] = 0
+			c.Data["LoggedUserName"] = ""
+			c.Data["IsAdmin"] = "no"
+		}
+
+		c.Data["CSRFToken"] = x.GetToken()
+		c.Data["CSRFTokenHTML"] = template.HTML(`<input type="hidden" name="_csrf" value="` + x.GetToken() + `">`)
+
+		ctx.Map(c)
+	}
+}
---
+++ vendor/github.com/MottainaiCI/mottainai-server/pkg/db/tiedot_user.go
@@ -0,0 +1,149 @@
+/*
+
+Copyright (C) 2017-2018  Ettore Di Giacinto <mudler@gentoo.org>
+Credits goes also to Gogs authors, some code portions and re-implemented design
+are also coming from the Gogs project, which is using the go-macaron framework
+and was really source of ispiration. Kudos to them!
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+package database
+
+import (
+	"errors"
+
+	user "github.com/MottainaiCI/mottainai-server/pkg/user"
+)
+
+var UserColl = "Users"
+
+func (d *Database) InsertAndSaltUser(t *user.User) (int, error) {
+	if err := t.SaltPassword(); err != nil {
+		return 0, err
+	}
+
+	return d.InsertUser(t)
+}
+
+func (d *Database) InsertUser(t *user.User) (int, error) {
+	return d.CreateUser(t.ToMap())
+}
+
+func (d *Database) CreateUser(t map[string]interface{}) (int, error) {
+
+	return d.InsertDoc(UserColl, t)
+}
+
+func (d *Database) DeleteUser(docID int) error {
+
+	t, err := d.GetUser(docID)
+	if err != nil {
+		return err
+	}
+
+	t.Clear()
+	return d.DeleteDoc(UserColl, docID)
+}
+
+func (d *Database) UpdateUser(docID int, t map[string]interface{}) error {
+	return d.UpdateDoc(UserColl, docID, t)
+}
+
+func (d *Database) SignIn(name, password string) (user.User, error) {
+	res, err := d.GetUsersByName(name)
+	if err != nil {
+		return user.User{}, err
+	}
+	if len(res) == 0 {
+		return user.User{}, errors.New("No username found")
+	}
+
+	u := res[0]
+	if ok, newSalt := u.VerifyPassword(password); ok {
+		if len(newSalt) > 0 {
+			err := d.UpdateUser(u.ID, map[string]interface{}{"password": newSalt})
+			if err != nil {
+				return u, errors.New("Error while updating salt:" + newSalt)
+			}
+		}
+		return u, nil
+	} else {
+		return user.User{}, errors.New("Wrong username or password")
+	}
+}
+
+func (d *Database) GetUserByName(name string) (user.User, error) {
+	res, err := d.GetUsersByName(name)
+	if err != nil {
+		return user.User{}, err
+	} else if len(res) == 0 {
+		return user.User{}, errors.New("No username found")
+	} else {
+		return res[0], nil
+	}
+}
+
+func (d *Database) GetUsersByName(name string) ([]user.User, error) {
+	var res []user.User
+
+	queryResult, err := d.FindDoc(UserColl, `[{"eq": "`+name+`", "in": ["name"]}]`)
+	if err != nil {
+		return res, err
+	}
+
+	for docid := range queryResult {
+
+		u, err := d.GetUser(docid)
+		u.ID = docid
+		if err != nil {
+			return res, err
+		}
+		res = append(res, u)
+	}
+	return res, nil
+}
+
+func (d *Database) GetUser(docID int) (user.User, error) {
+	doc, err := d.GetDoc(UserColl, docID)
+	if err != nil {
+		return user.User{}, err
+	}
+	t := user.NewUserFromMap(doc)
+	t.ID = docID
+	return t, err
+}
+
+func (d *Database) ListUsers() []DocItem {
+	return d.ListDocs(UserColl)
+}
+
+// TODO: Change it, expensive for now
+func (d *Database) CountUsers() int {
+	return len(d.ListUsers())
+}
+
+func (d *Database) AllUsers() []user.User {
+	Users := d.DB().Use(UserColl)
+	Users_id := make([]user.User, 0)
+
+	Users.ForEachDoc(func(id int, docContent []byte) (willMoveOn bool) {
+		t := user.NewUserFromJson(docContent)
+		t.ID = id
+		Users_id = append(Users_id, t)
+		return true
+	})
+	return Users_id
+}
